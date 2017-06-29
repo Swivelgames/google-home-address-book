37339dbc6b1318d37b0d506fcff13bbd018fa168
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2017-06-28 23:23:16 -0500

    Bug fixes - Fixed issue where Actions SDK was not being properly instantiated - Fixed issue where express `all` was erroneously being referenced as `any` - Renamed `action.json` as `gactions.json` for clarity

--- src/entry.js
+++ src/entry.js
@@ -2,7 +2,7 @@ import { ActionsSdkApp } from 'actions-on-google';
 import * as itents from './intents.js';
 
 export default (response, request) => {
-	const actionsApp = ActionsSdkApp({ response, request });
+	const actionsApp = new ActionsSdkApp({ response, request });
 	const actions = new Map();
 
 	itents.forEach((a) => actions.set(a.name, a.handler));
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import bodyparser from 'body-parser';
 import entry from './entry.js';
 
 const app = express();
-app.on('*', (...args) => entry(...args));
+app.all('/', (...args) => entry(...args));
 app.listen(3030);
 
 console.log('Listening on port 3030...');
